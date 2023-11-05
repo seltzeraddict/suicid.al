b3cdd227f67f899f284af29b859b2b9df3398e17
seltzeraddict <kyle.f.tansley@gmail.com>
2023-11-05 16:20:29 -0500

    Update: Random button fixes.

--- randomButton.js
+++ randomButton.js
@@ -3,8 +3,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const link = document.getElementById('randomBtn');
     link.addEventListener('click', (e) => {
         e.preventDefault(); // Prevent the default anchor action
-        const sections = ['section1', 'section2', 'section3']; // Add all your section ids here
+        const sections = ['post1', 'post2', 'post3', 'post4']; // Add all your section ids here
         const randomSection = sections[Math.floor(Math.random() * sections.length)];
         location.hash = `#${randomSection}`;
     });
 });
+
