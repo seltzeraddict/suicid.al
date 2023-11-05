004a29c06ad8383b19e20e979fdb028e509974cb
seltzeraddict <kyle.f.tansley@gmail.com>
2023-11-05 17:12:57 -0500

    New Content: Foliage and Loon photos/haikus added.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # suicid.al
-my lil art blog - photos, haikus, and music. 
+my lil art blog - photos, haikus, and music
 
 submit email in form to get updates when i post a new Thing
+
+thank you AKEP for letting me buy this edgelord domain 
--- randomButton.js
+++ randomButton.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const link = document.getElementById('randomBtn');
     link.addEventListener('click', (e) => {
         e.preventDefault(); // Prevent the default anchor action
-        const sections = ['post1', 'post2', 'post3', 'post4']; // Add all your section ids here
+        const sections = ['post1', 'post2', 'post3', 'post4', 'post5', 'post6']; // Add all your section ids here
         const randomSection = sections[Math.floor(Math.random() * sections.length)];
         location.hash = `#${randomSection}`;
     });
