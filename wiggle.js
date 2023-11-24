bc0ca577401b60e19127438a18eec01251271df6
seltzeraddict <kyle.f.tansley@gmail.com>
2023-11-24 13:54:43 -0500

    Update: Image ckeanup and + Song.

--- wiggle.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.menu_item').forEach((item) => {
-const text = item.textContent;
-const letters = [...text].map((letter) => {
-    return letter.trim() ? `<span>${letter}</span>` : '&nbsp;';
-}).join('');
-item.innerHTML = letters;
-});
